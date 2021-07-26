aca6b7dd1fb051f2ee40c263a884152de4483beb
Afeyer <afeyer@h5base.cn>
2021-07-26 17:38:16 +0800

    优化消息同步

--- syncmsg/callback.go
+++ syncmsg/callback.go
@@ -3,7 +3,7 @@ package syncmsg
 type Event struct {
 	ToUserName string `json:"to_user_name"`					// 微信客服组件ID
 	CreateTime int `json:"create_time"`						// 消息创建时间，unix时间戳
-	MsgType	string `json:"msgtype"`						// 消息的类型，此时固定为 event
+	MsgType	string `json:"msgtype"`							// 消息的类型，此时固定为 event
 	Event string `json:"event"`								// 事件的类型，此时固定为 kf_msg_or_event
 	Token string `json:"token"`								// 调用拉取消息接口时，需要传此token，用于校验请求的合法性
 }
