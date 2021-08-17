c8bf00f4ddc7fd21d2934732f397fdd4608cdd96
afeyer <1500527791@qq.com>
2021-08-17 16:45:33 +0800

    polish:客服链接支持自定义参数

--- syncmsg/message.go
+++ syncmsg/message.go
@@ -3,8 +3,8 @@ package syncmsg
 // BaseMessage 接收消息
 type BaseMessage struct {
 	MsgID string `json:"msgid"`								// 消息ID
-	OpenKFID string `json:"open_kfid"`						// 客服帐号ID
-	ExternalUserID string `json:"external_userid"`			// 客户UserID
+	OpenKFID           string `json:"open_kfid"`       		// 客服帐号ID（msgtype为event，该字段不返回）
+	ExternalUserID     string `json:"external_userid"` 		// 客户UserID（msgtype为event，该字段不返回）
 	ReceptionistUserID string `json:"servicer_userid"`		// 接待客服userID
 	SendTime uint64 `json:"send_time"`						// 消息发送时间
 	Origin uint32 `json:"origin"`							// 消息来源。3-客户回复的消息 4-系统推送的消息 5-客服回复消息
@@ -119,6 +119,7 @@ type EnterSessionEvent struct {
 		OpenKFID string `json:"open_kfid"`				// 客服账号ID
 		ExternalUserID string `json:"external_userid"`	// 客户UserID
 		Scene string `json:"scene"`						// 进入会话的场景值，获取客服帐号链接开发者自定义的场景值
+		SceneParam	   string `json:"scene_param"`	   // 进入会话的自定义参数，获取客服帐号链接返回的url，开发者按规范拼接的scene_param参数
 	} `json:"event"`									// 事件消息
 }
 
